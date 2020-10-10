217d9534bfe6f6ca77e36d6472b6a443563ce8f5
nico <24nico@gmail.com>
2020-10-10 00:39:09 -0400

    Update school_grades.py

--- school_grades.py
+++ school_grades.py
@@ -1,5 +1,25 @@
+import sys
+import os
+
+#Remember the problem we were having before where no directory is found?
+#ill show you how to fix that
+#you want to change this file's working directory to the one where it is located
+#Whenever you have this issue of: No such file or directory: 'numbers.csv'
+#make sure to enter the collowing code to change the directory to the script directory
+
+abspath = os.path.abspath(sys.argv[0])
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+#make sure to have numbers.csv in the same folder
+#run like this:
+#python nested.py numbers.csv 
+
+### Program ###
+
 #Opens the csv file and stores the text
-file = open('numbers.csv','r') 
+filename = sys.argv[1] #takes command line argument 
+file = open(filename,'r') 
 text = file.read()
 file.close()
 
@@ -8,17 +28,14 @@ text = text.strip()
 
 #Splits the text into it's corresponding rows
 rows = text.split("\n")
+#rows == [['22,36,82,20'],['89,21,23,134']]
 
-#after that: rows == [['22,36,82,20'],['89,21,23,134']]
 #Splits rows by their commas and stores them back in the same place
-
 for i in range(0,len(rows)):
   rows[i]=rows[i].split(',')
-    
-#after that: rows == [['22', '36', '82', '20'], ['89', '21', '23', '134']]
+#rows == [['22', '36', '82', '20'], ['89', '21', '23', '134']]
 
 ##let's convert the strings to floats
-
 for row in rows:
   for element in row:
     current_row_i = rows.index(row)
@@ -27,19 +44,15 @@ for row in rows:
 
 
 big_list = []
-
 for row in rows: 
+  small_list = [] #small list resets every time
   for element in row:
-    small_list = [] #small list resets every time
     if(element<65): #grading limit
       small_list.append(row.index(element)) #list.index(element) will return the index of that element
-    big_list.append(small_list) #now you append small_list to big_list which unlike small_list, doesn't reset
-    
-#after that: big_list == [[0,1,3],[1,2]]
+  big_list.append(small_list) #now you append small_list to big_list which unlike small_list, doesn't reset
+#big_list == [[0,1,3],[1,2]]
 
-#Now print the elements inside big_list (in this case the 2 lists)
-
+#Now print the elements (lists) inside big_list (in this case the 2 lists)
 for l in big_list:
   print(l)
-  
   
